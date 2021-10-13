af1b3322ed1cad0ee0e5111f3190112276d1d276
VinayagamD <vinaygam.d.ganesh@gmail.com>
2021-10-13 20:23:07 +0530

    Intial Code For Demo

---
+++ Test.java
@@ -0,0 +1,22 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello To Java");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+
+}
+
+class Test1 {
+
+}
+
+class Test2{
+
+}
+
+class  Test3{
+
+}
